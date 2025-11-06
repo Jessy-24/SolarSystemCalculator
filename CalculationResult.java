4f8d90ca1cf146be14de911484922f60efeae392
Jessy-24 <jessicaescobanez9@gmail.com>
2025-11-06 21:23:24 +0800

    Add files via upload

---
+++ CalculationResult.java
@@ -0,0 +1,8 @@
+public class CalculationResult {
+    public double totalDailyWh;
+    public double pvW;
+    public double batteryAh;
+    public double inverterW;
+    public double controllerA;
+    public double peakLoadW;
+}
